2a40ebd9bea031e93c22f81bd0ea99a2450154ae
ajithbhaskaran4 <ajithbhaskaran4@gmail.com>
2023-09-07 21:45:45 +0530

    changes

---
+++ BackEnd/Image2PointCloud.py
@@ -0,0 +1,70 @@
+import numpy as np
+import os
+from PIL import Image
+import pyvista as pv
+import natsort 
+import matplotlib.pyplot as plt
+
+
+cmap = plt.get_cmap('viridis')
+
+
+class Image2PointCloud:
+    def __init__(self):
+        self.directory = [] #r'P:\MRI_Cyst\archive\kaggle_3m\TCGA_HT_A61A_20000127'  #TCGA_HT_A61A_20000127 TCGA_HT_8107_19980708
+        self.images = np.empty((0, 256, 256, 3))
+        self.pointcloud = []
+        self.color = []
+        
+    def setPaths(self, path):
+        self.directory = path
+        
+    def read_mri_images(self):
+        self.images = np.empty((0, 256, 256, 3))
+        directoryList = os.listdir(self.directory)
+        #print("Unsorted : ",directoryList)
+        directoryList = natsort.natsorted(directoryList,reverse=False)
+        #print("Sorted: ", directoryList)
+        for filename in os.listdir(self.directory):
+            if not filename.endswith("_mask.tif"): 
+                #print("Image: ", filename)
+                img_path = os.path.join(self.directory, filename)
+                img = np.asarray(Image.open(img_path))  # Read the image in grayscale
+                img = np.expand_dims(img, axis=0)
+                self.images = np.append(self.images, img, axis = 0)
+                
+    def getnumberofImages(self):
+        return self.images.shape[0]
+                
+    def convert2PointCloud(self):
+        points = []
+        colors = []
+        transparency = []
+        scale_factor = 0.1
+        
+        points = np.argwhere(self.images[:,:,:,1])
+        colors = cmap(self.images[points[:, 0], points[:, 1], points[:, 2], :]/255)
+        transparency = self.images[points[:, 0], points[:, 1], points[:, 2], 1]/255
+        
+        points = np.array(points)
+        print("point shape: ", points.shape)
+        transparency = np.array(transparency).astype(float)
+        print("transparency shape: ", transparency.shape)
+        colors = np.array(colors)
+        print("colors shape: ", colors.shape)
+        
+        print("Point Cloud Completed")
+        self.point_cloud = pv.PolyData(points)
+        print("Point Cloud color")
+        self.point_cloud['point_color'] = colors
+        print("Point Cloud transparecy")
+        self.point_cloud['transparency'] = transparency
+        #mesh = self.point_cloud.delaunay_3d()
+        print("meshing completed")
+        return self.point_cloud
+                   
+        
+    def get_StackMRI(self):
+        print("Image stack Size: ", self.images.shape)
+        return self.images
+    
---
+++ Main_GUI.py
@@ -0,0 +1,74 @@
+import streamlit as st
+from BackEnd.Image2PointCloud import Image2PointCloud
+from stpyvista import stpyvista
+import pyvista as pv
+import  streamlit_vertical_slider  as svs
+from PIL import Image
+import numpy as np
+import tempfile
+import os
+
+st.set_page_config(layout="wide")
+
+# ipythreejs does not support scalar bars :(
+pv.global_theme.show_scalar_bar = False
+
+if 'flag' not in st.session_state:
+    st.session_state.flag = False
+    
+if 'backend' not in st.session_state:
+    st.session_state.backend = Image2PointCloud()
+st.title("MRI CYST ANALYSIS")
+st.text("An Application to view and quantify the presence of cyst from MRI")
+
+def change_MRI():
+    with MRICol:
+        st.subheader("MRI Image")
+        currentImage = st.session_state.ImageStack[st.session_state.MRI_Slider, :,:,:]
+        print("current Image size: ", currentImage.shape)
+        currentImage = Image.fromarray(np.uint8(currentImage), mode = "RGB")
+        st.image(currentImage)
+               
+uploaded_files  = st.file_uploader("Choose the MRI Images", accept_multiple_files=True, key = 'Folder_Path')
+
+if uploaded_files:
+    temp_dir = tempfile.mkdtemp()
+    st.write("Uploaded Files:")
+    for file in uploaded_files:
+        file_name = file.name
+        file_path = os.path.join(temp_dir, file_name)
+        # Save the uploaded file with its original name to the temporary directory
+        with open(file_path, "wb") as f:
+            f.write(file.read())
+    st.session_state.backend.setPaths(temp_dir)
+
+
+if st.button('Submit'):
+    st.session_state.backend.read_mri_images()
+    #st.session_state.pointCloud = backend.convert2PointCloud()
+    #st.session_state.NumImages = backend.getnumberofImages()
+    #st.session_state.ImageStack = backend.get_StackMRI()
+    if 'pointCloud' not in st.session_state:
+        st.session_state.pointCloud = st.session_state.backend.convert2PointCloud()
+    if 'NumImages' not in st.session_state:
+        st.session_state.NumImages = st.session_state.backend.getnumberofImages()
+    if 'ImageStack' not in st.session_state:
+        st.session_state.ImageStack = st.session_state.backend.get_StackMRI()
+    print("received all data")
+    st.session_state.flag = True
+
+
+
+if st.session_state.flag == True:
+
+    pointCol, intermediate, MRICol = st.columns([3, 1,3])
+    with pointCol:
+        st.subheader("3D MRI View")
+        st.slider("Select MRI Image Slice", min_value=0, max_value=st.session_state.NumImages, step=1, key = "MRI_Slider",on_change=change_MRI)
+        plotter = pv.Plotter(window_size=[600,600])
+        plotter.add_points(st.session_state.pointCloud, opacity = st.session_state.pointCloud['transparency'], cmap='bone') #, opacity = pointCloud['transparency']
+        plotter.add_scalar_bar()
+        plotter.view_isometric()
+        plotter.background_color = 'white'
+        stpyvista(plotter, key="MRI")
+            
