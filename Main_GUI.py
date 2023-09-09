b483d34502f9279a958c98fe5daba11144fcad86
ajithbhaskaran4 <ajithbhaskaran4@gmail.com>
2023-09-10 00:10:29 +0530

    binary

--- Main_GUI.py
+++ Main_GUI.py
@@ -95,10 +95,10 @@ if st.button('Submit'):
 if st.session_state.flag == True:
     st.session_state.sliderPos = st.slider("Select MRI Image Slice", min_value=1, max_value=st.session_state.NumImages, step=1, key = "MRI_Slider")
     #temp_dir = tempfile.mkdtemp()
-    st.session_state.pointCloud.save(os.path.join(temp_dir, "pointCloud.stl"))
+    st.session_state.pointCloud.save(os.path.join(temp_dir, "pointCloud.stl"), binary = True)
     if os.path.exists(os.path.join(temp_dir, "pointCloud.stl")):
         st.write("File Exist")
-    with open(os.path.join(temp_dir, "pointCloud.stl"), 'r') as file:
+    with open(os.path.join(temp_dir, "pointCloud.stl"), 'wb') as file:
         st.download_button(label="Download MRI 3D structure", data=file, file_name='MRI_3D.stl')
     mri_image, MRI_Cyst = st.columns([1,1])
     with mri_image:
