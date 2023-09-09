9398237333e540012e5c4c403823901747862912
ajithbhaskaran4 <ajithbhaskaran4@gmail.com>
2023-09-09 23:45:42 +0530

    change

--- Main_GUI.py
+++ Main_GUI.py
@@ -96,7 +96,7 @@ if st.session_state.flag == True:
     st.session_state.sliderPos = st.slider("Select MRI Image Slice", min_value=1, max_value=st.session_state.NumImages, step=1, key = "MRI_Slider")
     temp_dir = tempfile.mkdtemp()
     st.session_state.pointCloud.save(os.path.join(temp_dir, "pointCloud.stl"))
-    with open(os.path.join(temp_dir, "pointCloud.stl"), 'rb') as file:
+    with open(os.path.join(temp_dir, "pointCloud.stl"), 'b') as file:
         st.download_button(label="Download MRI 3D structure", data=file, file_name='MRI_3D.stl')
     mri_image, MRI_Cyst = st.columns([1,1])
     with mri_image:
