204963055ec0180cd4a926ac08777f30b1d8ecea
ajithbhaskaran4 <ajithbhaskaran4@gmail.com>
2023-09-10 11:44:17 +0530

    changes

--- Main_GUI.py
+++ Main_GUI.py
@@ -95,7 +95,7 @@ if st.button('Submit'):
 if st.session_state.flag == True:
     st.session_state.sliderPos = st.slider("Select MRI Image Slice", min_value=1, max_value=st.session_state.NumImages, step=1, key = "MRI_Slider")
     #temp_dir = tempfile.mkdtemp()
-    st.session_state.pointCloud.save(os.path.join(temp_dir, "pointCloud.stl"), binary = True)
+    st.session_state.pointCloud.reconstruct_surface().save(os.path.join(temp_dir, "pointCloud.stl"), binary = True)
     if os.path.exists(os.path.join(temp_dir, "pointCloud.stl")):
         st.write("File Exist")
     with open(os.path.join(temp_dir, "pointCloud.stl"), 'rb') as file:
