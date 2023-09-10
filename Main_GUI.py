e16a72aa7f09a9ec031c2f205b89adafec22b493
ajithbhaskaran4 <ajithbhaskaran4@gmail.com>
2023-09-10 12:30:35 +0530

    changes

--- Main_GUI.py
+++ Main_GUI.py
@@ -95,7 +95,7 @@ if st.button('Submit'):
 if st.session_state.flag == True:
     st.session_state.sliderPos = st.slider("Select MRI Image Slice", min_value=1, max_value=st.session_state.NumImages, step=1, key = "MRI_Slider")
     #temp_dir = tempfile.mkdtemp()
-    st.session_state.pointCloud.reconstruct_surface().save(os.path.join(temp_dir, "pointCloud.stl"))
+    st.session_state.pointCloud.delaunay_3d(alpha=2.).save(os.path.join(temp_dir, "pointCloud.stl"))
     print("Meshing completed")
     if os.path.exists(os.path.join(temp_dir, "pointCloud.stl")):
         st.write("File Exist")
