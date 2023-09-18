f254ea11903fd074dff0ea2dd8ae26761ba21ebf
ajithbhaskaran4 <ajithbhaskaran4@gmail.com>
2023-09-18 14:40:33 +0530

    jhvjh

--- BackEnd/Image2PointCloud.py
+++ BackEnd/Image2PointCloud.py
@@ -106,7 +106,7 @@ class Image2PointCloud:
         
         points = np.argwhere(self.edges[:,:,:,0]>=20)
         colors = cmap(self.images[points[:, 0], points[:, 1], points[:, 2], :]/255)
-        transparency = np.ones(points.shape[0])*0.8
+        transparency = np.ones(points.shape[0])*0.01
        
         
         points = np.array(points)
